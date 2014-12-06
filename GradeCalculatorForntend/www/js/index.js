321c27463cf9e97cece915d3df2ef4b69c283c37
randeepsingh85 <randeepsingh85@gmail.com>
2014-12-05 23:24:58 -0800

    Integrating with backend code

--- GradeCalculatorForntend/www/js/index.js
+++ GradeCalculatorForntend/www/js/index.js
@@ -16,6 +16,21 @@
 	      });
 	};
 	
+  var toast=function(msg){
+  	$("<div class='ui-loader ui-overlay-shadow ui-body-e ui-corner-all'><h3>"+msg+"</h3></div>")
+  	.css({ display: "block", 
+  		opacity: 0.90, 
+  		position: "fixed",
+  		padding: "7px",
+  		"text-align": "center",
+  		width: "270px",
+  		left: ($(window).width() - 284)/2,
+  		top: $(window).height()/2 })
+  	.appendTo( $.mobile.pageContainer ).delay( 1500 )
+  	.fadeOut( 400, function(){
+  		$(this).remove();
+  	});
+    };
 	
 	var doSignup = function() {
 		submitAjaxForm("#frmSignup","signup.php",
@@ -24,12 +39,12 @@
 			var obj = JSON.parse(data);
 			if(obj.success)
 				{
-					alert('Signup successful');
+					toast('Signup successful');
 					$.mobile.changePage("#index");
 				}
 			else
 				{
-					alert(obj.error_message);
+					toast(obj.error_message);
 				}
 		}
 		,
@@ -39,13 +54,36 @@
 	
 	var doLogin = function()
 	{
-		
-	}
+		submitAjaxForm("#login_form","login.php",
+		function(data,status)
+		{
+      console.log(data);
+			var obj = JSON.parse(data);
+			if(obj.success)
+				{
+					toast('Signup successful');
+					$.mobile.changePage("#homepage");
+				}
+			else
+				{
+					toast(obj.error_message);
+				}
+		}
+		,
+		null
+		);
+	};
 	
+  $(document).bind("mobileinit", function(){
+    $.mobile.allowCrossDomainPages = true;
+  });
+  
+  
 	// Setup the event handlers
 	$(document).on("ready", function() {
+    
 		$.grV = new Object()
-		$.grV.baseURL = "http://localhost/Grade/GradeCalculatorBackend/GradeCalculator/";
+		$.grV.baseURL = "http://bay-area.net/cmpe235/proj/";
 		
 		$('#btn_do_Signup').on('click',doSignup);
 		$('#do_login').on('click',doLogin);

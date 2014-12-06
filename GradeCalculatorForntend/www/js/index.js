740c6ca8911a40a1c851683c7ec0ac5d378fec84
randeepsingh85 <randeepsingh85@gmail.com>
2014-12-06 04:47:18 -0800

    Integration code more

--- GradeCalculatorForntend/www/js/index.js
+++ GradeCalculatorForntend/www/js/index.js
@@ -19,7 +19,7 @@
   var toast=function(msg){
   	$("<div class='ui-loader ui-overlay-shadow ui-body-e ui-corner-all'><h3>"+msg+"</h3></div>")
   	.css({ display: "block", 
-  		opacity: 0.90, 
+  		opacity: 0.50, 
   		position: "fixed",
   		padding: "7px",
   		"text-align": "center",
@@ -50,6 +50,7 @@
 		,
 		null
 		);
+		return false;
 	};
 	
 	var doLogin = function()
@@ -57,13 +58,13 @@
 		submitAjaxForm("#login_form","login.php",
 		function(data,status)
 		{
-      console.log(data);
 			var obj = JSON.parse(data);
 			if(obj.success)
 				{
-					toast('Signup successful');
+          $.grV.user = obj.data;
+					//toast('Signup successful');
 					$.mobile.changePage("#homepage");
-				}
+    			}
 			else
 				{
 					toast(obj.error_message);
@@ -72,7 +73,92 @@
 		,
 		null
 		);
+		return false;
 	};
+  
+  var loadProfile = function()
+  {
+	$.mobile.changePage("#profilepage");
+	    
+	//Assign values to user profle page
+	  $('#user_id').val($.grV.user.user_student_id);
+	  $('#fname').val($.grV.user.user_first_name);
+	  $('#lname').val($.grV.user.user_last_name);
+	  $('#email').val($.grV.user.user_email);
+	  $('#phone').val($.grV.user.user_phone_number);
+	  
+	  return false;
+  };
+  
+  var launchGrader = function()
+  {
+	  //user_type = 1, student
+	  //user_type = 0, instrcutor
+	  
+	  if($.grV.user.user_type == 1)
+	  {
+	  	$.mobile.changePage("#studcomputegrade");
+	  }
+	  else if($.grV.user.user_type == 0)
+	  {
+	  	$.mobile.changePage("#profcomputegrade");
+	  }
+	  return false;
+  };
+  
+  var studComputeGrade = function()
+  {
+	
+	$('#studGradeForm').append('<input type="hidden" name="user_email" value='+ $.grV.user.user_email +'>');  
+	submitAjaxForm("#studGradeForm","compute_grade.php",
+		function(data,status)
+		{
+			var obj = JSON.parse(data);
+			console.log("$$$$$$$$$$$ " + obj.grade);
+			if(obj.success)
+				{
+					$('#stud_lgarde').html("<h2>Grade: " + obj.grade + "</h2>");
+	  			}
+			else
+				{
+					toast(obj.error_message);
+				}
+		}
+		,
+		null
+	 );
+	return false;
+  };
+  
+  var laodCourseList = function()
+  {
+	$.ajax({
+          type: "GET",
+          url: $.grV.baseURL+"get_classes.php",
+          cache: false,
+          dataType: "text",
+          data: null,
+          success: function(data, status){
+  			var obj = JSON.parse(data);
+  			if(obj.success)
+  				{
+  					$.mobile.changePage("#courselist");
+					$('#courselistcontent').empty();
+					for(var x in obj.data)
+					{
+						var ele = $('<p style="text-align: center;"><button class="ui-btn ui-btn-b" id='+obj.data[x].class_number+'>'+obj.data[x].class_number+'</button></p>');
+						$('#courselistcontent').append(ele)
+					}
+  	  			}
+  			else
+  				{
+  					toast(obj.error_message);
+  				}
+          },
+          error: null
+      });
+	  return false;
+  };
 	
   $(document).bind("mobileinit", function(){
     $.mobile.allowCrossDomainPages = true;
@@ -87,7 +173,14 @@
 		
 		$('#btn_do_Signup').on('click',doSignup);
 		$('#do_login').on('click',doLogin);
+    	$('#profile_info').on('click', loadProfile);
+		$('#speed_grader').on('click', launchGrader);
+		$('#stud_compute_grade').on('click', studComputeGrade);
+		$('#course_list').on('click', laodCourseList);
 		
+		$('#back_profilepage').on('click', function(){$.mobile.changePage("#homepage"); return false;})
+		$('#back_courselist').on('click', function(){$.mobile.changePage("#homepage"); return false;})
+		$('#back_studgrader').on('click', function(){$.mobile.changePage("#homepage"); return false;})
 		
 	});
 
